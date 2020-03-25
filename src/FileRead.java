b9c378a15f213cfd990de29d57095ba4a05edfaf
thetechconspiracy <thetechconspiracy@outlook.com>
2020-03-25 12:01:12 -0400

    Work on review class

--- src/FileRead.java
+++ src/FileRead.java
@@ -3,8 +3,12 @@
  * Description
  */
 
+//https://www.geeksforgeeks.org/parse-json-java/
+
+import java.io.FileNotFoundException;
+import org.json.simple.*;
 
 
 public class FileRead {
-
+  //TODO: Code
 }
--- src/Review.java
+++ src/Review.java
@@ -2,7 +2,56 @@
  * Written by Something Creative
  * Description
  */
-package PACKAGE_NAME;
+
+import java.text.NumberFormat;
 
 public class Review {
+  private String title;
+  private String contents;
+  private int rating;
+
+  public Review(String title, String contents, int rating){
+    setReview(title, contents, rating);
+  }
+
+  public Review(String[] review) {
+    setReview(review);
+  }
+
+  public Review(int rating){
+    if(rating <= 5 && rating > 0){//Bounds checking
+      this.rating = rating;
+    }
+  }
+  public void setReview(String title, String contents, int rating){
+    this.title = title;
+    this.contents = contents;
+    if(rating <= 5 && rating > 0){//Bounds checking
+      this.rating = rating;
+    }
+  }
+
+  public void setReview(String[] review){
+    if (review.length != 3) { return; }
+
+    String title = review[0];
+    String contents = review[1];
+    String rating = review[2];
+
+    if (rating.length() != 1) { return; }
+
+    int ratingInt;
+    try {
+      ratingInt = Integer.parseInt(rating);
+    } catch (Exception NumberFormatException) {
+      return;
+    }
+
+    if (ratingInt > 5 || ratingInt < 1) { return; }
+
+    this.title = title;
+    this.contents = contents;
+    this.rating = ratingInt;
+  }
+
 }
