fbe09fd5ef5a5699ad044c4ca8a6a221d7727d04
unknown <beremer@email.sc.edu>
2020-04-01 19:49:16 -0400

    added print functionality for a consumer's ticket

--- src/Ticket.java
+++ src/Ticket.java
@@ -4,25 +4,51 @@
  * This is the ticket class to keep track of the date for an event
  *
  */
+import java.io.*;
 import java.time.*;
+import java.util.*;
 
 public class Ticket {
 //TODO add event variable
-	//private Event event;
+	private Event event;
 	private LocalTime time;
 	private LocalDate date;
 	
-	/*
+	
 	public void setEvent(Event event) {
 		
+		this.event = event;
+		
 	}
-	*/
 	
-	/*
+	
+	
 	public Event getEvent() {
 		return this.event;
 	}
-	*/
+	
+	/**
+	 * prints the ticket to a text file
+	 */
+	public void printTicket() {
+		try {
+			FileWriter filewriter = new FileWriter("c:/temp/ticket.txt");
+			PrintWriter printwriter = new PrintWriter(filewriter);
+			printwriter.println("**************************************");
+			printwriter.print(this.event.getTitle());
+			//TODO get individual date and time for an event
+			printwriter.println(this.event.getRating());
+			printwriter.println(this.event.getDes());
+			printwriter.println(this.event.getCastString());
+			printwriter.println("**************************************");
+			printwriter.close();
+		}
+		catch(Exception e) {
+			System.out.println("Could not successfully write to the file");
+		}
+		
+	}
+	
 	
 	/**
 	 * 
