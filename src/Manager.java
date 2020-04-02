9adaea03a7285772d9dd5dcc96204c8ca7bde133
unknown <beremer@email.sc.edu>
2020-04-02 18:16:19 -0400

    added method to allow manager to add movies, plays, and concerts to a venue

--- src/Manager.java
+++ src/Manager.java
@@ -14,9 +14,45 @@ public class Manager implements User {
 	private HashMap password;
 	
 	/**
-	 * 
+	 * adds an event to a venue
 	 */
-	public void addMovie() {
+	public void addEvent(Theater theater) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("What would you like to add?\n1: Movie\n2: Play\n3: Concert");
+		int choice = keyboard.nextInt();
+		switch(choice)
+		{
+		case 1:
+			System.out.println("Enter the movie's title: ");
+			String movieTitle = keyboard.nextLine();
+			System.out.println("Enter the movie's description: ");
+			String movieDes = keyboard.nextLine();
+			System.out.println("Enter the movie's rating: ");
+			String movieRating = keyboard.nextLine();
+			theater.addEvent(new Movie(movieTitle, movieDes, movieRating));
+			break;
+		case 2:
+			System.out.println("Enter the movie's title: ");
+			String playTitle = keyboard.nextLine();
+			System.out.println("Enter the movie's description: ");
+			String playDes = keyboard.nextLine();
+			System.out.println("Enter the movie's rating: ");
+			String playRating = keyboard.nextLine();
+			theater.addEvent(new Play(playTitle, playDes, playRating));
+			break;
+		case 3:
+			System.out.println("Enter the movie's title: ");
+			String concertTitle = keyboard.nextLine();
+			System.out.println("Enter the movie's description: ");
+			String concertDes = keyboard.nextLine();
+			System.out.println("Enter the movie's rating: ");
+			String concertRating = keyboard.nextLine();
+			theater.addEvent(new Play(concertTitle, concertDes, concertRating));
+			break;
+		default:
+			System.out.println("Invalid Choice");
+		}
 		
 	}
 	
